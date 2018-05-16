b68d572a5ec87cb9dc1e5f05a917d606ab3ab5b4
Hafiz Saad <muhammad.saad@now.net.pk>
2018-05-16 12:55:15 +0500

    Added Controller classes for All the screens in both storyboards

---
+++ PostSafety/PostSafety/ViewControllers/SendVerificationCodeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SendVerificationCodeViewController.swift
+//  PostSafety
+//
+//  Created by Hafiz Saad on 16/05/2018.
+//  Copyright © 2018 Now Tel. All rights reserved.
+//
+
+import UIKit
+
+class SendVerificationCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func backButtonTouched(_ sender: UIButton)
+    {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
