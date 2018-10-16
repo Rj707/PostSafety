690637dbca5b48bc8245e4383e046d576b388792
Raja Saad <rajasaad707@gmail.com>
2018-10-16 17:31:01 +0500

    Login screen autofill issue - The login screen should not load any info into phone number or password by default - it should be blank

--- PostSafety/PostSafety/ViewControllers/Authentication/PSLogInViewController.swift
+++ PostSafety/PostSafety/ViewControllers/Authentication/PSLogInViewController.swift
@@ -32,8 +32,11 @@ class PSLogInViewController: UIViewController
     
     func configureAndInitialize()
     {
-        self.phoneNumberTextField?.text = "4038709552"
-        self.passowrdTextField?.text = "123456"
+//        self.phoneNumberTextField?.text = "4034790605"
+////        4034790605
+////        4038709552
+////        2222222222
+//        self.passowrdTextField?.text = "123456"
         
         var text: NSMutableAttributedString? = nil
         if let aText = termsLabel?.attributedText
